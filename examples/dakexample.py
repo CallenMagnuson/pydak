921937f0525c1b32ae5c72948580d6d8d5a0191a
CallenMagnuson <callen@magnuson.us>
2021-03-12 09:30:34 -0600

    Added Baseball, Changed Serial COM port

--- examples/dakexample.py
+++ examples/dakexample.py
@@ -1,13 +1,15 @@
-from daktronics import DakUDP, Daktronics
+from daktronics import DakSerial, Daktronics
 
 if __name__ == '__main__':
     print("UDP MULTICAST 21000")
-    dak_data = DakUDP(21000)
-    dak = Daktronics("hockey/lacrosse", dak_data)
+    dak_data = DakSerial()
+    dak = Daktronics("baseball", dak_data)
     while True:
         dak.update()
         print("--------------------------------------------------------------")
-        print(dak['Main Clock Time (mm:ss/ss.t )'])
-        print(dak['Home Team Name'], dak['Home Team Score'])
-        print(dak['Guest Team Name'], dak['Guest Team Score'])
+        print("Ball: "+dak['Ball'])
+        print("Strike: "+dak['Strike'])
+        print("Out: "+dak['Out'])
+        print("Inning: "+dak['Inning'])
+        print("Home Hits: "+dak['Home Hits'])
         print("--------------------------------------------------------------")
